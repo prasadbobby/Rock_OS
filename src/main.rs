c2f3d7d50908f2cd732f46fc2ba030307afd3ae5
Harshit Ruwali <harshitruwali@outlook.com>
2020-07-24 12:06:53 +0530

    working
    Signed-off-by: Harshit Ruwali <harshitruwali@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle] 
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function named `_start` by default
+    loop {}
 }
+
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// fn main() {
+//     // println!("Hello, world!");
+// }
